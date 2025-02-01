a27e00341c8b9a409ea0244d51294568e4792941
Roman Prokopchuk <ghost62727@gmail.com>
2025-02-01 19:53:20 +0200

    Implement HS algorithm

--- distributed_systems_of_information_processing/Lab_1/main.py
+++ distributed_systems_of_information_processing/Lab_1/main.py
@@ -1,8 +1,33 @@
+import math
+from enum import Enum
+
+class Direction(Enum):
+    LEFT = 1,
+    RIGHT = 2
+
+    def inverse(self):
+        return self.LEFT if self.value == self.RIGHT else self.RIGHT
+
+class Message:
+    def __init__(self, data, direction, hops_count=1, ack_leader=False):
+        self.data = data
+        self.direction = direction
+        self.hops_count = hops_count
+        self.ack_leader = ack_leader
+
+    def __str__(self):
+        return f'Message[data={self.data}, direction={self.direction}, hops_count={self.hops_count}, ack_leader={self.ack_leader}]'
+
 class Process:
     def __init__(self, id):
         self.id = id
+        self.is_leader = False
         self.left_process = None
         self.right_process = None
+        self.is_round_finished_from_left = False
+        self.is_round_finished_from_right = False
+        self.round = 0
+        self.is_acknowledged_about_leader = False
 
     def set_left(self, left_process):
         self.left_process = left_process
@@ -13,25 +38,76 @@ class Process:
     def __str__(self):
         return f'Process {self.id}: left_process_id={self.left_process.id}, right_process_id={self.right_process.id}'
 
-    def send_to_left(self, data):
-        print(f'Process {self.id} sent to left neighbour process with id {self.left_process.id} message: {data}')
-        self.left_process.receive_message(self.id, data)
+    def send(self, data, hops_count, direction, ack_leader=False):
+        if direction == Direction.LEFT:
+            self.send_to_left(data, hops_count, ack_leader)
+        else:
+            self.send_to_right(data, hops_count, ack_leader)
 
-    def send_to_right(self, data):
-        print(f'Process {self.id} sent to right neighbour process with id {self.right_process.id} message: {data}')
-        self.right_process.receive_message(self.id, data)
+    def send_to_left(self, data, hops_count, ack_leader=False):
+        print(f'Process {self.id} sent to left neighbour process with id {self.left_process.id} message: {data} and hops_count: {hops_count}')
+        self.left_process.receive_message(self.id, Message(data, Direction.LEFT, hops_count, ack_leader))
 
-    def receive_message(self, sender_id, data):
-        print(f'Process {self.id} received message from process {sender_id} message: {data}')
-        # TODO: impl
+    def send_to_right(self, data, hops_count, ack_leader=False):
+        print(f'Process {self.id} sent to right neighbour process with id {self.right_process.id} message: {data} and hops_count: {hops_count}')
+        self.right_process.receive_message(self.id, Message(data, Direction.RIGHT, hops_count, ack_leader))
+
+    def receive_message(self, sender_id, message):
+        if self.is_acknowledged_about_leader:
+            return
+
+        print(f'Process {self.id} received message from process {sender_id} message: {message}')
+        cur_hops_count = message.hops_count - 1
+
+        if message.ack_leader:
+            self.__handle_leader_acknowledgment(message.data, message.direction)
+            return
+
+        if cur_hops_count == 0:
+            self.__handle_message_return(sender_id, message)
+            return
+
+        if message.data > self.id:
+            self.send(message.data, cur_hops_count, message.direction)
+        elif message.data == self.id:
+            self.is_leader = True
+            print(f'Process {self.id} becomes the leader!')
+            self.__notify_about_leader()
+
+    def __handle_leader_acknowledgment(self, leader_id, direction):
+        print(f'Process {self.id} received acknowledgment that process {leader_id} is leader!')
+        self.is_acknowledged_about_leader = True
+        self.send(leader_id, 1, direction, True)
+
+
+    def __handle_message_return(self, sender_id, message):
+        if message.data == self.id:
+            if message.direction == Direction.LEFT:
+                self.is_round_finished_from_right = True
+            else:
+                self.is_round_finished_from_left = True
+
+            if self.is_round_finished_from_left and self.is_round_finished_from_right:
+                self.round += 1
+                print(f'Process {self.id} moves to the next round: {self.round}')
+
+                self.send_to_left(self.id, 2 ** self.round)
+                self.send_to_right(self.id, 2 ** self.round)
+
+        else:
+            self.send(message.data, 1, message.direction)
+
+    def __notify_about_leader(self):
+        self.send_to_left(self.id, 1 , True)
+        self.send_to_right(self.id, 1 , True)
 
 
 class HSElectionDemo:
-    def __init__(self, proceses_count=4):
-        if proceses_count <= 1:
+    def __init__(self, processes_count=4):
+        if processes_count <= 1:
             raise ValueError('Process count must be greater than 1')
 
-        self.processes_count = proceses_count
+        self.processes_count = processes_count
         self.__init_processes()
 
     def __init_processes(self):
@@ -42,16 +118,25 @@ class HSElectionDemo:
 
         for i in range(self.processes_count):
             current_process = self.processes[i]
-            left_process_idx = i - 1 if i != 0 else self.processes_count - 1
-            right_process_idx = i + 1 if i != self.processes_count - 1 else 0
+            left_process_idx = (i - 1) % self.processes_count
+            right_process_idx = (i + 1) % self.processes_count
 
             current_process.set_left(self.processes[left_process_idx])
             current_process.set_right(self.processes[right_process_idx])
 
     def start_election(self):
-        print('Election started')
-        for p in self.processes:
-            print(p)
+        self.__print_processes()
+        print(f'=====================Election started. Process count: {self.processes_count}=====================')
+
+        init_hops_count = 1
+        for process in self.processes:
+            process.send_to_left(process.id, init_hops_count)
+            process.send_to_right(process.id, init_hops_count)
+
+    def __print_processes(self):
+        print('=====================Processes=====================')
+        for process in self.processes:
+            print(process)
 
 
 def main():
